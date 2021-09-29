83cf0c789ac786fae245dc40a6231b61bb6876ca
mendel11a <57116911+mendel11a@users.noreply.github.com>
2021-09-29 21:42:27 +0300

    code design

--- src/components/BarChart.js
+++ src/components/BarChart.js
@@ -1,25 +1,20 @@
 import React from 'react';
 import { Bar, Line, Pie, Doughnut } from 'react-chartjs-2';
 
-
+const colorsArray= ["#f38b4a","#56d798","#ff8397","#6970d5","#00ffff"]
+const countriesArray =['Israel', 'Italy', 'Spain', 'Sweden', 'Germany']
 function Charts(chartData) {
     return (
         <>
             <div className="flex-container">
                 <div className="flex-child bar-chart">
-
                     <Bar
                         data={{
-                            labels: ['Israel', 'Italy', 'Spain', 'Sweden', 'Germany'],
+                            labels:countriesArray ,
                             datasets: [
                                 {
                                     label: 'Confirmed',
-                                    backgroundColor:
-                                        ["#f38b4a",
-                                            "#56d798",
-                                            "#ff8397",
-                                            "#6970d5",
-                                            "#00ffff"],
+                                    backgroundColor:colorsArray,
                                     data: [chartData.data[0].Confirmed, chartData.data[1].Confirmed, chartData.data[2].Confirmed,
                                     chartData.data[3].Confirmed, chartData.data[4].Confirmed]
                                 },
@@ -39,16 +34,11 @@ function Charts(chartData) {
                 <div className="flex-child pie-chart">
                     <Pie
                         data={{
-                            labels: ['Israel', 'Italy', 'Spain', 'Sweden', 'Germany'],
+                            labels: countriesArray,
                             datasets: [
                                 {
                                     label: 'Deaths',
-                                    backgroundColor:
-                                        ["#f38b4a",
-                                            "#56d798",
-                                            "#ff8397",
-                                            "#6970d5",
-                                            "#00ffff"], 
+                                    backgroundColor:colorsArray, 
                                     data: [chartData.data[0].Deaths, chartData.data[1].Deaths, chartData.data[2].Deaths,
                                             chartData.data[3].Deaths, chartData.data[4].Deaths]
                                 },
@@ -71,16 +61,11 @@ function Charts(chartData) {
                 <div className="flex-child bar-chart">
                     <Doughnut
                         data={{
-                            labels: ['Israel', 'Italy', 'Spain', 'Sweden', 'Germany'],
+                            labels:countriesArray,
                             datasets: [
                                 {
                                     label: 'Active',
-                                    backgroundColor:
-                                        ["#f38b4a",
-                                            "#56d798",
-                                            "#ff8397",
-                                            "#6970d5",
-                                            "#00ffff"],
+                                    backgroundColor:colorsArray,
                                     data: [chartData.data[0].Active, chartData.data[1].Active, chartData.data[2].Active,
                                     chartData.data[3].Active, chartData.data[4].Active]
                                 },
@@ -101,16 +86,11 @@ function Charts(chartData) {
                 <div className="flex-child pie-chart">
                     <Line
                         data={{
-                            labels: ['Israel', 'Italy', 'Spain', 'Sweden', 'Germany'],
+                            labels:countriesArray,
                             datasets: [
                                 {
                                     label: 'Recovered',
-                                    backgroundColor:
-                                        ["#f38b4a",
-                                            "#56d798",
-                                            "#ff8397",
-                                            "#6970d5",
-                                            "#00ffff"], 
+                                    backgroundColor:colorsArray, 
                                             data: [chartData.data[0].Recovered, chartData.data[1].Recovered, chartData.data[2].Recovered,
                                             chartData.data[3].Recovered, chartData.data[4].Recovered]
                                 },
